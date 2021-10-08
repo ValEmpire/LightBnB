fae3eac07388922d36c11739645ae97793420673
ValEmpire <palma_arcival@yahoo.com>
2021-10-08 19:24:16 +0000

    create 1 queries

---
+++ 1_queries/city_total_reservations.sql
@@ -0,0 +1,4 @@
+SELECT city, count(properties.id) AS total_reservations FROM properties
+JOIN reservations ON reservations.property_id = properties.id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
